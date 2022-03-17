ee335f060dbc633436f4fae807ecf4d4406bfb4a
Loki <uddeeptaraajkashyap@gmail.com>
2022-03-17 19:11:22 +0000

    updated mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -4,6 +4,8 @@ import subprocess
 import optparse
 import re
 
+from flask_login import current_user
+
 def get_arguments():
     parser= optparse.OptionParser()
 
@@ -25,19 +27,32 @@ def change_mac(interface,new_mac):
 
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig", interface, "up"])\
+
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig",interface])
+
+    regex = r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w'
+    mac_address_search_result = re.search(regex, str(ifconfig_result))
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
 
 
 options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+
+print("Current MAC: " + str(current_mac))
+
 change_mac(options.interface,options.new_mac)
 
-ifconfig_result = subprocess.check_output(["ifconfig",options.interface])
+current_mac=get_current_mac(options.interface)
 
-regex = r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w'
-mac_address_search_result = re.search(regex, str(ifconfig_result))
-
-if mac_address_search_result:
-    print(mac_address_search_result.group(0))
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to "+ current_mac)
 else:
-    print("[-] Could not read MAC address.")
-
+    print("[+] MAC address did not get changed")
